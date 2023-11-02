7b0c313a4f3ab78bd38f23f15f3d84956d6c80e5
Brian <bvw20002010@gmail.com>
2023-11-02 01:50:54 -0400

    add

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup as bs4
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 main_url = "https://www.dead.net/"
 
-album = "Veneta"
+album = "WOTF"
 
 if album == "Veneta":
     dropDown = "songSelect desktop songs-two"
